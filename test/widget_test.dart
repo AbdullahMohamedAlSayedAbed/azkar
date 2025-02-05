d020e6166bb1243e04c204badf99ff3e6d14579c
Abdullah Abed <abedabdallah834@gmail.com>
2025-02-05 20:07:31 +0200

    Skip widget test execution temporarily in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:azkar/main.dart';
-@Skip('Skipping test for now')
 void main() {
   testWidgets('Azkar app test', (WidgetTester tester) async {
 
